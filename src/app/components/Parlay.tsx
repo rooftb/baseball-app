997602318c02a4c632566b3ca907cacf7057293a
rooftb <tanner.roof@outlook.com>
2025-03-27 21:32:53 -0400

    fix: updated ts errors

--- src/app/components/Parlay.tsx
+++ src/app/components/Parlay.tsx
@@ -1,78 +1,73 @@
-import { useEffect, useState } from 'react'
-import styles from "../page.module.css";
-import { convertToMoneyline } from '../utils/functions';
-import { GameCardType } from '../interfaces/types';
+// import { useEffect, useState } from 'react'
+// import styles from "../page.module.css";
+// import { convertToMoneyline } from '../utils/functions';
+// import { GameCardType } from '../interfaces/types';
 
-export default function Parlay({ data, index }: GameCardType) {
+// export default function Parlay({ data, index }: GameCardType) {
 
-    const [favoredTeamOddsArray, setFavoredTeamsOddsArray] = useState();
+//     const [favoredTeamOddsArray, setFavoredTeamsOddsArray] = useState();
 
+//     useEffect(() => {
 
-    useEffect(() => {
+//         if (!data) { return; }
 
-        if (!data) { return; }
+//         if (data !== undefined) {
+//             console.log(data.matchups + ' parlay component')
+//         }
 
-        if (data !== undefined) {
-            console.log(data.matchups + ' parlay component')
-        }
+//     }, [data])
 
-    }, [data])
+//     const [money, setMoney] = useState(10);
 
+//     var betcruncher, slip, selections;
 
+//     betcruncher = require("../utils/betcruncher");
 
-    const [money, setMoney] = useState(10);
+//     slip = {
+//         type: "single",
+//         stake: money,
+//         eachWay: false
+//     };
 
-    var betcruncher, slip, selections;
+//     selections = [
+//         { odds: "+735", terms: "1/4", position: 1 }
+//     ];
 
-    betcruncher = require("../utils/betcruncher");
+//     console.log(betcruncher(slip, selections).returns); // { totalStake: 10, returns: 110, profit: 100 }
 
-    slip = {
-        type: "single",
-        stake: money,
-        eachWay: false
-    };
+//     console.log(data)
 
-    selections = [
-        { odds: "+735", terms: "1/4", position: 1 }
-    ];
+//     useEffect(() => {
+//         if (data) {
+//             console.log(data.matchups)
+//             // console.log('favored team odds array ' + favoredTeamOddsArray)
 
-    console.log(betcruncher(slip, selections).returns); // { totalStake: 10, returns: 110, profit: 100 }
+//             // console.log('favored team odds converted to parlay odds ' + +convertToParlayOdds(favoredTeamOddsArray))
 
-    console.log(data)
+//             data.matchups.map((item, index) => {
 
+//                 const home_team = data[index].matchups[0].odds[3].markets[0].outcomes[0].price;
+//                 const away_team = data[index].matchups[0].odds[3].markets[0].outcomes[0].price;
 
-    useEffect(() => {
-        if (data) {
-            console.log(data.matchups)
-            // console.log('favored team odds array ' + favoredTeamOddsArray)
+//                 if (home_team < away_team) {
+//                     setFavoredTeamsOddsArray(home_team)
+//                 } else {
+//                     setFavoredTeamsOddsArray(away_team)
+//                 }
+//             });
+//         }
+//     }, [data])
 
-            // console.log('favored team odds converted to parlay odds ' + +convertToParlayOdds(favoredTeamOddsArray))
+//     function convertToParlayOdds(favoredTeamOddsArray: number[]) {
+//         return favoredTeamOddsArray.reduce((a: number, b: number) => a * b).toFixed(2)
+//     }
 
-            data.matchups.map((item, index) => {
+//     return (
+//         <>
+//             {/* {
+//                 data.matchups && <span>Parlay all {data.matchups.length} ML: {convertToMoneyline(+convertToParlayOdds(favoredTeamOddsArray))}</span>
+//             } */}
+//             <span>$<input className={styles.input} name="money" value={money} onChange={(e) => setMoney(+e.target.value)} /> to win ${betcruncher(slip, selections).returns.toFixed(2)}</span>
+//         </>)
 
-                const home_team = data[index].matchups[0].odds[3].markets[0].outcomes[0].price;
-                const away_team = data[index].matchups[0].odds[3].markets[0].outcomes[0].price;
-
-                if (home_team < away_team) {
-                    setFavoredTeamsOddsArray(home_team)
-                } else {
-                    setFavoredTeamsOddsArray(away_team)
-                }
-            });
-        }
-    }, [data])
-
-
-    function convertToParlayOdds(favoredTeamOddsArray: number[]) {
-        return favoredTeamOddsArray.reduce((a: number, b: number) => a * b).toFixed(2)
-    }
-
-    return (
-        <>
-            {/* {
-                data.matchups && <span>Parlay all {data.matchups.length} ML: {convertToMoneyline(+convertToParlayOdds(favoredTeamOddsArray))}</span>
-            } */}
-            <span>$<input className={styles.input} name="money" value={money} onChange={(e) => setMoney(+e.target.value)} /> to win ${betcruncher(slip, selections).returns.toFixed(2)}</span>
-        </>)
-
-}
+// }
